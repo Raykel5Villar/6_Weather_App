317479bcadf048ee0f5b44e14d3849564ab648a3
Raykel5Villar <raykelvillar@gmail.com>
2024-05-08 22:21:34 -0400

    creating html files

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, about):
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
